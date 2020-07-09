efc29bfc1c72f1f3984388794b2315b0bd55559f
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-07-08 19:45:09 -0700

    added first round of inquirer questions about manager

--- app.js
+++ app.js
@@ -13,6 +13,29 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your manager's name?",
+        name: "name",
+    },
+    {
+        type: "input",
+        message: "What is your manager's id?",
+        name: "id",
+    },
+    {
+        type: "input",
+        message: "What is your manager's email?",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What is your manager's office number?",
+        name: "officeNumber",
+    },
+])
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
