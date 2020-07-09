8089df11c1301947bdb4c54d07e2c64be8ddb3d2
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-07-08 22:27:40 -0700

    Passes tests

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const { run } = require("jest");
 
+
+//Set variable for array of employee answer data
+const employeeList = [];
+
+//Also variable for htmlPage render function to work
+let htmlPage;
+
+
 //Set object for standard employee questions
 const employeeQuestions = [
     {
@@ -62,7 +70,53 @@ function employeePrompt(employeeType) {
         default:
             break;
     }
-};
+}
+//Keep asking if the user wants to add another employee 
+employeeQuestions.push({
+    type: "list",
+    message: "Do you want to add another employee?",
+    name: "employeeType",
+    choices: [
+        "Engineer",
+        "Intern",
+        "None"
+    ],
+})
+
+//push the user answers into the employeeList object
+inquirer.prompt(employeeQuestions).then(function (answers) {
+    switch (employeeType) {
+        case "Manager":
+            employeeList.push(new Manager(answers.name, answers.id, answers.email, answers.officeNumber))
+            break;
+        case "Engineer":
+            employeeList.push(new Engineer(answers.name, answers.id, answers.email, answers.github))
+            break;
+        case "Intern":
+            employeeList.push(new Intern(answers.name, answers.id, answers.email, answers.school))
+            break;
+        default:
+            break;
+    }
+
+    //Once the user selects "none", move to render the html page
+    if (answers.employeeType !== "None") {
+        employeePrompt(answers.employeeType);
+    }
+    else {
+        // call html renderer to write the final html file in the output folder
+        console.log("Calling render");
+        htmlPage = render(employeeList);
+
+        fs.writeFile("output/team.html", htmlPage, function (error) {
+            if (error) {
+                return console.log(error);
+            }
+            console.log("Success! See your team html page");
+        });
+    }
+});
+
 
 
 
